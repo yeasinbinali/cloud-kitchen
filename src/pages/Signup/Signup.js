d487b8c60bb0c4503cda7543656f123d969498a7
yeasinbinali <yeasinbinali@gmail.com>
2023-03-26 16:38:24 +0600

    signup and login page setup

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import login from "../../images/signIn&Out/login&signup.svg";
 
 const Signup = () => {
-    return (
-        <div>
-            <h2>Signup</h2>
+  return (
+    <div className="hero py-10">
+      <div className="hero-content grid md:grid-cols-2 gap-20">
+        <div className="lg:w-96 md:w-80">
+          <img src={login} alt="" />
         </div>
-    );
+        <div className="card w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <h1 className="text-5xl font-bold text-center">Signup</h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Your Name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Signup</button>
+            </div>
+            <p className='text-center mt-4'>Already have an account? <Link className='text-red-900' to='/login'>Login</Link></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
