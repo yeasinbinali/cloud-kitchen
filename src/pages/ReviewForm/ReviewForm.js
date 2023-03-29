8c9f3f96809dfc79cccb8fa6f73e010b87934162
yeasinbinali <yeasinbinali@gmail.com>
2023-03-30 00:20:03 +0600

    post method added in reviewform

--- src/pages/ReviewForm/ReviewForm.js
+++ src/pages/ReviewForm/ReviewForm.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router';
-import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/UserContext';
 
 const ReviewForm = () => {
     const {_id, name, price} = useLoaderData();
+    const {user} = useContext(AuthContext);
+
+    const handleReviewForm = event => {
+        event.preventDefault();
+        const form = event.target;
+        const fullName = form.name.value;
+        const image = form.image.value;
+        const email = user?.email || 'unregistered';
+        const price = form.price.value;
+        const message = form.message.value;
+
+        const review = {
+            reviewId: _id,
+            reviewerName: fullName,
+            title: name,
+            image,
+            email,
+            price,
+            message
+        }
+
+        fetch('http://localhost:5000/reviews', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(review)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.acknowledged){
+                form.reset();
+            }
+        })
+        .catch(err => console.error(err))
+    }
     
     return (
-        <form className='text-center'>
+        <div>
+            <form onSubmit={handleReviewForm} className='text-center'>
             <h2 className='text-3xl font-bold my-5'>{name}</h2>
             <div>
-                <input type="text" placeholder="Your name" className="input input-bordered w-full max-w-xs md:mr-1 mb-1" required/>
-                <input type="text" placeholder="Your Image URL" className="input input-bordered w-full max-w-xs" required/>
+                <input name='name' type="text" placeholder="Your name" className="input input-bordered w-full max-w-xs md:mr-1 mb-1" required/>
+                <input name='image' type="text" placeholder="Your Image URL" className="input input-bordered w-full max-w-xs" required/>
             </div>
             <div className='my-1'>
-                <input type="text" placeholder="Email" className="input input-bordered w-full max-w-xs  md:mr-1 mb-1" />
-                <input type="text" defaultValue={`$${price}`} className="input input-bordered w-full max-w-xs" readOnly/>
+                <input name='email' defaultValue={user?.email} type="text" placeholder="Email" className="input input-bordered w-full max-w-xs  md:mr-1 mb-1" />
+                <input name='price' type="text" defaultValue={`$${price}`} className="input input-bordered w-full max-w-xs" readOnly/>
             </div>
             <div>
-                <textarea className="textarea textarea-bordered w-full md:max-w-xl max-w-xs text-base" placeholder="Please write down your review here"></textarea>
+                <textarea name='message' className="textarea textarea-bordered w-full md:max-w-xl max-w-xs text-base" placeholder="Please write down your review here"></textarea>
             </div>
-            <Link><button className="btn btn-primary mt-3">Submit</button></Link>
+            <input type='submit' className='btn btn-primary mt-3' value='Submit'  />
         </form>
+        </div>
     );
 };
 
